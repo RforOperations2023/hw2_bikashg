38959273083383d84a93cbd43182bac6ae1f88ba
Bikash Gupta <bikashg@andrew.cmu.edu>
2023-02-15 00:20:46 -0500

    update, further polished
    Cleaned to make it more presentable.

--- hw2_shinydashboard/app.R
+++ hw2_shinydashboard/app.R
@@ -5,12 +5,21 @@ library(dplyr)
 library(plotly)
 library(shinythemes)
 
+# Next steps:
+# 1. Appropriate Ids: output and input
+# 2. Figure out why info box do not buzz 
+# 3. Learn about scatter plot, pie chart, and heat map that you can generate
+# 4. Use 1st tab to provide info and include images
+# 5. Learn more about reactivity 
+# 6. Learn from slides, cheatsheet, and other resources online 
+
 # Load and clean data ----------------------------------------------
 
 country_profile <- read.csv('selected_country.csv')
 
 country_profile <- country_profile %>%
-  mutate(Region = as.factor(Region))
+  mutate(Region = as.factor(Region),
+         country = as.factor(country))
 
 # Avoid plotly issues ----------------------------------------------
 pdf(NULL)
@@ -21,7 +30,7 @@ header <- dashboardHeader(title = "Country Profile Dashboard",
 
                           # Drop down menu with hard coded values ------------------------------
                           dropdownMenu(type = "notifications",
-                                       notificationItem(text = "Interested in International Development, apply here", 
+                                       notificationItem(text = "Saci Suta approved your request", 
                                                         icon = icon("users"))
                           ),
                           dropdownMenu(type = "tasks", badgeStatus = "success",
@@ -47,7 +56,7 @@ sidebar <- dashboardSidebar(
     
     # Inputs: select variables to plot ----------------------------------------------
     selectInput("worldSelect",
-                "Homeworld:",
+                "Geographical Regions:",
                 choices = sort(unique(country_profile$Region)),
                 multiple = TRUE,
                 selectize = TRUE,
@@ -70,6 +79,7 @@ body <- dashboardBody(tabItems(
   # Plot page ----------------------------------------------
   tabItem("plot",
           
+          # What is this?
           # Input and Value Boxes ----------------------------------------------
           fluidRow(
             infoBoxOutput("mass"),
@@ -80,15 +90,15 @@ body <- dashboardBody(tabItems(
           fluidRow(
             tabBox(title = "Plot",
                    width = 12,
-                   tabPanel("Mass", plotlyOutput("plot_mass")),
-                   tabPanel("Height", plotlyOutput("plot_height")))
+                   tabPanel("Food Production", plotlyOutput("plot_mass")),
+                   tabPanel("Women Leaders", plotlyOutput("plot_height")))
           )
   ),
   
   # Data Table Page ----------------------------------------------
   tabItem("table",
           fluidPage(
-            box(title = "Selected Character Stats", DT::dataTableOutput("table"), width = 12))
+            box(title = "Selected Country Stats", DT::dataTableOutput("table"), width = 12))
   )
 )
 )
@@ -102,10 +112,10 @@ server <- function(input, output) {
   swInput <- reactive({
     country_sub <- country_profile %>%
       
-      # Slider Filter ----------------------------------------------
+    # GDP Per Capita Filter ----------------------------------------------
     filter(GDP.per.capita..current.US.. >= input$birthSelect[1] & GDP.per.capita..current.US.. <= input$birthSelect[2])
     
-    # Homeworld Filter ----------------------------------------------
+    # Region Filter ----------------------------------------------
     if (length(input$worldSelect) > 0 ) {
       country_sub <- subset(country_profile, Region %in% input$worldSelect)
     }
@@ -117,21 +127,25 @@ server <- function(input, output) {
   # Reactive melted data ----------------------------------------------
   mwInput <- reactive({
     swInput() %>%
-      melt(id = "country")
+      melt(id = "country") # should I be melting by country? 
   })
   
   # A plot showing the mass of characters -----------------------------
   output$plot_mass <- renderPlotly({
-    dat <- subset(mwInput(), variable == "Population.density..per.km2..2017.")
+    dat <- subset(mwInput(), variable == "Food.production.index..2004.2006.100.")
     
     # Generate Plot ----------------------------------------------
-    ggplot(data = dat, aes(x = country, y = as.numeric(value), fill = country)) + geom_bar(stat = "identity")
+    ggplot(data = dat, aes(x = country, y = as.numeric(value), fill = country)) + geom_bar(stat = "identity") + 
+      labs(y = 'Food Production Index', x = 'Country') + theme(axis.text.x=element_text(angle=45,hjust=1,vjust=0.5))
   })
   
-  # A plot showing the height of characters -----------------------------------
+  # A plot showing seats held by women in national parliament -----------------------------------
   output$plot_height <- renderPlotly({
     dat <- subset(mwInput(),  variable == "Seats.held.by.women.in.national.parliaments..")
-    ggplot(data = dat, aes(x = country, y = as.numeric(value), fill = country)) + geom_bar(stat = "identity")
+    
+    ggplot(data = dat, aes(x = country, y = as.numeric(value), fill = country)) + geom_bar(stat = "identity") + 
+      labs(y = 'Seats Held by Women in National Parliament', x = 'Country') + 
+      theme(axis.text.x=element_text(angle=45,hjust=1,vjust=0.5))
   })
   
   # Data table of characters ----------------------------------------------
@@ -139,20 +153,22 @@ server <- function(input, output) {
     subset(swInput(), select = c(country, Region, Population.density..per.km2..2017.))
   })
   
+  #
   # Mass mean info box ----------------------------------------------
   output$mass <- renderInfoBox({
     sw <- swInput()
-    num <- round(mean(country_profile$Food.production.index..2004.2006.100., na.rm = T), 2)
+    num <- round(mean(sw$Food.production.index..2004.2006.100., na.rm = T), 2)
     
-    infoBox("Avg Mass", value = num, subtitle = paste(nrow(sw), "characters"), icon = icon("balance-scale"), color = "purple")
+    infoBox("Avg Food Production", value = num, subtitle = paste(nrow(sw), "characters"), 
+            icon = icon("balance-scale"), color = "purple")
   })
   
   # Height mean value box ----------------------------------------------
   output$height <- renderValueBox({
     sw <- swInput()
-    num <- round(mean(country_profile$Seats.held.by.women.in.national.parliaments.., na.rm = T), 2)
+    num <- round(mean(sw$Seats.held.by.women.in.national.parliaments.., na.rm = T), 2)
     
-    valueBox(subtitle = "Avg Height", value = num, icon = icon("sort-numeric-asc"), color = "green")
+    valueBox(subtitle = "Women Leaders Number", value = num, icon = icon("sort-numeric-asc"), color = "green")
   })
 }
 
